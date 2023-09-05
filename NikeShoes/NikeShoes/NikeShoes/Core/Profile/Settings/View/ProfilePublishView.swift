fd74bf66282b2f34185ae9b6d541d8e2ed77a8f9
pHH <gudghks56@gmail.com>
2023-09-05 20:12:41 +0900

    conflict

---
+++ NikeShoes/NikeShoes/NikeShoes/Core/Profile/Settings/View/ProfilePublishView.swift
@@ -0,0 +1,118 @@
+//
+//  ProfilePublishView.swift
+//  NikeShoes
+//
+//  Created by 김성훈 on 2023/09/05.
+//
+
+import SwiftUI
+
+struct ProfilePublishView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var title: String
+    
+    var body: some View {
+        VStack {
+            Rectangle()
+                .fill(Color.gray)
+                .frame(height: 130)
+                .overlay {
+                    Text("개인 정보 보호 설정이 모든 Nike 앱에 적용됩니다. 하나의 앱에서 설정을 변경하면 모든 앱의 설정이 변경됩니다.")
+                        .font(.medium16)
+                        .padding(.vertical)
+                }
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("공개 (모두에게)")
+                        .font(.bold16)
+                        .padding(.vertical)
+                
+                    Text("모든 나이키 멤버가 나를 검색하고, 나의 전체 프로필을 보고, 초대를 보내고, 나의 활동을 확인할 수 있습니다.")
+                        .font(.medium16)
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+                
+                Button {
+                    // TODO: 범위 설정 로직
+                } label: {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.black)
+                }
+                .padding(.horizontal)
+            }
+            
+            Divider()
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("소셜 (친구만)")
+                        .font(.bold16)
+                        .padding(.vertical)
+                
+                    Text("모든 나이키 멤버가 나를 검색하고, 나의 제한된 프로필을 보고, 초대를 보낼 수 있습니다. 친구들만이 나의 전체 프로필을 보고 나의 활동을 확인할 수 있습니다. 그룹에 공유한 활동은 그룹 내 다른 구성원들도 볼 수 있습니다.")
+                        .font(.medium16)
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+                Button {
+                    // TODO: 범위 설정 로직
+                } label: {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.black)
+                }
+                .padding(.horizontal)
+            }
+            
+            Divider()
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("비공개 (나만)")
+                        .font(.bold16)
+                        .padding(.vertical)
+                
+                    Text("어떤 사용자도 나를 검색하거나 나의 프로필 및 활동을 볼 수 없습니다. 친구를 추가할 수 없습니다. 소셜 네트워크에 활동을 공유하면, 해당 네트워크에 연결된 모든 친구가 이활동을 볼 수 있습니다.")
+                        .font(.medium16)
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+                Button {
+                    // TODO: 범위 설정 로직
+                } label: {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.black)
+                }
+                .padding(.horizontal)
+            }
+            
+            Divider()
+            
+            Spacer()
+        }
+        .lineSpacing(4)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    self.presentationMode.wrappedValue.dismiss()
+                }) {
+                    Image(systemName: "chevron.left")
+                }
+                .foregroundColor(.black)
+            }
+        }
+        .navigationBarTitle(title, displayMode: .inline)
+        .navigationBarBackButtonHidden()
+    }
+}
+
+struct ProfilePublishView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfilePublishView(title: "프로필 공개 설정")
+    }
+}
---
+++ NikeShoes/NikeShoes/NikeShoes/Core/Profile/Settings/View/SignOutView.swift
@@ -0,0 +1,83 @@
+//
+//  SignoutView.swift
+//  NikeShoes
+//
+//  Created by 김성훈 on 2023/09/05.
+//
+
+import SwiftUI
+
+struct SignOutView: View {
+    var body: some View {
+        List {
+            VStack(alignment: .leading) {
+                Text("계정을 삭제하시겠습니까?")
+                    .font(.semiBold24)
+                    .padding(.vertical)
+                
+                
+                Text("계정을 삭제할 경우:")
+                
+                HStack(alignment: .top) {
+                    Image(systemName: "link")
+                        .frame(width: 40)
+                    Text("동일한 이메일 주소 및 전화번호를 사용하는 관련 나이키 앱 계저의 모든 데이터를 잃게 됩니다. 여기에는 NRC, NTC 및 SNKRS가 포함됩니다")
+                }
+                .padding(.vertical)
+                
+                HStack(alignment: .top) {
+                    Image(systemName: "link")
+                        .frame(width: 40)
+                    Text("무료 배송, 빠른 결제, 멤버 전용 상품, NRC 러닝 가이드, NTC 운동 프로그램 등 나이키 멤버 혜택을 이용할 수 없게 됩니다.")
+                }
+                .padding(.vertical)
+                
+                HStack(alignment: .top) {
+                    Image(systemName: "link")
+                        .frame(width: 40)
+                    Text("나이키 제휴사 사이트, 장비, 미디어 및 컬래버레이션(예: Garmin, Apple Watch등) 연결이 끊기게 됩니다")
+                }
+                .padding(.vertical)
+                    
+                Text("계정을 삭제하면 이 앱에서 로그아웃됩니다. 다른 나이키 앱에 로그인한 경우, 세션이 만료되기까지 최대 1시간이 소요될 수 있습니다.\n\n계정 삭제 후 주문정보에 액세스 하려면 나이키 고객 서비스팀에 문의해 주세요.\n\n마음이 바뀌면 언제든지 돌아오셔서 새로운 나이키 계정을 만들 수 있습니다.\n")
+                
+                Text("계정을 삭제하시겠습니까? (취소는 불가능 합니다)\n")
+                    .bold()
+                
+                Button {
+                    
+                } label: {
+                    HStack {
+                        // TODO: 나중에 분기 처리 필요
+                        Image(systemName: "square")
+                            .font(.medium20)
+                        Image(systemName: "checkmark.square.fill")
+                            .font(.medium20)
+                        Text("예, 계정을 삭제하겠습니다.")
+                    }
+                }
+                .padding(.bottom)
+                
+                Text("요청을 제출하시면 계정을 비활성화해 드립니다. 데이터를 완전히 삭제 및 제거하기까지는 최대 10일이 소요될 수 있습니다.")
+                    .font(.medium12)
+                
+                CustomButton(background: .black, foregroundColor: .white, title: "계정 삭제") {
+                    //
+                }
+                
+//                CustomButton(background: .white, foregroundColor: .black, title: "취소") {
+//                    //
+//                }
+//                .border(.gray)
+            
+            }
+        }
+        .listStyle(.plain)
+    }
+}
+
+struct SignoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignOutView()
+    }
+}
