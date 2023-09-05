05ae051742e311cc26168e85cc2d199fba4007b4
lhchan4010 <lhchan4010@gmail.com>
2023-09-05 09:40:38 +0900

    profile home

---
+++ NikeShoes/NikeShoes/NikeShoes/Core/Profile/Home/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,42 @@
+import Foundation
+import SwiftUI
+
+enum ProfileViewModel: Int, CaseIterable {
+    case orders
+    case pass
+    case events
+    case settings
+    
+    var title: String {
+        switch self {
+        case .orders: return "oreders"
+        case .pass: return "pass"
+        case .events: return "events"
+        case .settings: return "settings"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .orders: return "creditcard.fill"
+        case .pass: return "hand.raised.square.on.square.fill"
+        case .events: return "calendar"
+        case .settings: return "gearshape"
+        }
+    }
+}
+
+extension ProfileViewModel {
+    var destinationView: some View {
+        switch self {
+        case .orders:
+            return AnyView(SettingsView())
+        case .pass:
+            return AnyView(SettingsView())
+        case .events:
+            return AnyView(SettingsView())
+        case .settings:
+            return AnyView(SettingsView())
+        }
+    }
+}
