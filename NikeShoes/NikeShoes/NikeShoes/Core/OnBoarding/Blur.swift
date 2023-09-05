50ab4e75a767a944631463d93dab7f198051015f
pHH <gudghks56@gmail.com>
2023-09-05 16:42:02 +0900

    feat: Blur 추가

--- NikeShoes/NikeShoes/NikeShoes/Core/OnBoarding/Blur.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/OnBoarding/Blur.swift
@@ -5,4 +5,19 @@
 //  Created by 박형환 on 2023/09/05.
 //
 
-import Foundation
+import SwiftUI
+
+
+struct Blur: UIViewRepresentable {
+    
+    var style: UIBlurEffect.Style = .systemMaterial
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+    
+}
