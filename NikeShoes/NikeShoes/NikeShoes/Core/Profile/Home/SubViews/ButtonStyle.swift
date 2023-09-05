5e7b048f15f1ba7964147bcc972e6753e4403933
pHH <gudghks56@gmail.com>
2023-09-05 17:51:21 +0900

    fix: preview 이름중복, pbxproj파일 '}'생략 된거 추가

---
+++ NikeShoes/NikeShoes/NikeShoes/Core/Profile/Home/SubViews/ButtonStyle.swift
@@ -0,0 +1,39 @@
+//
+//  TempButton.swift
+//  NikeShoes
+//
+//  Created by 박형환 on 2023/09/05.
+//
+
+import SwiftUI
+
+
+struct ButtonStyle: View {
+    
+    @State var buttonText: String = "View in Bag"
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 31.5)
+                    .foregroundColor(Color.white)
+                    .frame(width: 351, height: 63)
+                    .overlay(RoundedRectangle(cornerRadius: 31.5)
+                        .stroke(Color.gray, lineWidth: 0.5))
+                
+                Text(buttonText)
+                    .font(.system(size: 18, weight: .medium))
+                    .foregroundColor(.black)
+            }
+            Spacer()
+        }
+    }
+        
+}
+
+
+struct ButtonStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonStyle()
+    }
+}
