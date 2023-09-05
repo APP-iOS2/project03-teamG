0f2089b9d39864dbba962218ca96b5babbea14e2
Chan Ho Park <park_ch@kakao.com>
2023-09-05 17:52:11 +0900

    Fix: 네비게이션 뷰 중첩으로 인한 오류 해결

--- NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/ShopView.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/ShopView.swift
@@ -9,31 +9,28 @@ import SwiftUI
 
 struct ShopView: View {
     var body: some View {
-        VStack{
-            ScrollView {
-                
-                AppBestCollectionView()
-                    .frame(height: 250)
-                    .padding(.bottom,20)
-                
+        ScrollView {
+            AppBestCollectionView()
+                .frame(height: 250)
                 LazyVStack {
-                    ForEach(0 ... 10, id: \.self) { _ in
-                        VStack{
-                            ZStack{
-                                Rectangle()
-                                    .frame(width: .infinity, height: 120)
-                                Text("카테고리")
-                                    .foregroundColor(Color.white)
+                    
+                    LazyVStack {
+                        ForEach(0 ... 10, id: \.self) { _ in
+                            VStack{
+                                ZStack{
+                                    Rectangle()
+                                        .frame(width: .infinity, height: 120)
+                                    Text("카테고리")
+                                        .foregroundColor(Color.white)
+                                }
                             }
                         }
-                        
                     }
                 }
-                
             }
         }
     }
-}
+
 
 struct ShopView_Previews: PreviewProvider {
     static var previews: some View {
