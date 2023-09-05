5bbe046ab5c2c267b1b1262b8bb249273b39edb3
pHH <gudghks56@gmail.com>
2023-09-05 16:46:46 +0900

    feat: 흰색 버튼 컴포넌트 추가

--- NikeShoes/NikeShoes/NikeShoes/Core/OnBoarding/TempButton.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/OnBoarding/TempButton.swift
@@ -5,4 +5,41 @@
 //  Created by 박형환 on 2023/09/05.
 //
 
-import Foundation
+import SwiftUI
+
+
+struct TempButton: View {
+    
+    var foreground: Color = .black
+    var background: Color = .white
+    let action: (() -> Void)?
+    
+    var body: some View {
+        Button(action: {
+            withAnimation {
+               action?()
+            }
+        }, label: {
+            Text("Get Started")
+                .bold()
+        })
+        .padding(.horizontal , 30)
+        .padding(.vertical , 20)
+        .background(background)
+        .foregroundColor(foreground)
+        .cornerRadius(30)
+    }
+}
+
+
+struct TempButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            
+            Color.black
+            TempButton {
+                
+            }
+        }
+    }
+}
