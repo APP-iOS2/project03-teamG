e670b1c338d7f9cd233b73fc71e6d1f1c867cc44
ahshotchoo <wjdchl@ewhain.net>
2023-09-06 20:34:53 +0900

    add: Category Views 추가

---
+++ NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/Category/KidsView.swift
@@ -0,0 +1,20 @@
+//
+//  KidsView.swift
+//  NikeShoes
+//
+//  Created by 최소정 on 2023/09/06.
+//
+
+import SwiftUI
+
+struct KidsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KidsView_Previews: PreviewProvider {
+    static var previews: some View {
+        KidsView()
+    }
+}
---
+++ NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/Category/MaleView.swift
@@ -0,0 +1,20 @@
+//
+//  MaleView.swift
+//  NikeShoes
+//
+//  Created by 최소정 on 2023/09/06.
+//
+
+import SwiftUI
+
+struct MaleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MaleView_Previews: PreviewProvider {
+    static var previews: some View {
+        MaleView()
+    }
+}
