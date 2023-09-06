a6ed5ff787b255bf8804487dc8e043bb7d4dc461
ahshotchoo <wjdchl@ewhain.net>
2023-09-06 10:16:13 +0900

    add: ShopView 하단 뷰 파일 생성

---
+++ NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/RecentItemView.swift
@@ -0,0 +1,20 @@
+//
+//  RecentItemView.swift
+//  NikeShoes
+//
+//  Created by 최소정 on 2023/09/06.
+//
+
+import SwiftUI
+
+struct RecentItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecentItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecentItemView()
+    }
+}
