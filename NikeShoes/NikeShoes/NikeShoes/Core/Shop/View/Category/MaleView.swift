6c75e972561671d904a1efc04a9862904e8d46ab
Chan Ho Park <park_ch@kakao.com>
2023-09-07 08:00:47 +0900

    Fix : 카테고리 "남성" 기능 추가
    1. 남성 카테고리에 이미지 추가
    2. 남성 카테고리에 네비게이션 링크들 추가 일단은 아이템 뷰로 이동
    3. 탭바 아이콘에 네이밍
    4. Shop 에 하나로 만들어진 카테고리 개별 파일로 분할

--- NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/Category/MaleView.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/Category/MaleView.swift
@@ -8,10 +8,44 @@
 import SwiftUI
 
 struct MaleView: View {
+    let categories = [
+        ("신제품", "MaleCategory1"),
+        ("전체보기", "MaleCategory2"),
+        ("조던", "MaleCategory3"),
+        ("에어", "MaleCategory4"),
+        ("덩크", "MaleCategory5"),
+        ("코르", "MaleCategory6")
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 3) {  // 간격을 3으로 설정
+            ForEach(categories, id: \.0) { category, imageName in
+                NavigationLink(destination: ItemListView()) {
+                    ZStack(alignment: .leading) {  // 왼쪽 정렬
+                        Rectangle()
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
+                        
+                        Image(imageName)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
+                            .clipped()
+                        
+                        Text(category)
+                            .font(.system(size: 20))
+                            .bold()  // 볼드 처리
+                            .foregroundColor(Color.white)
+                            .padding(.leading, 50)  // 왼쪽으로부터 50만큼 떨어지게 배치
+                    }
+                }
+            }
+        }
     }
 }
+
+
+
+
 
 struct MaleView_Previews: PreviewProvider {
     static var previews: some View {
