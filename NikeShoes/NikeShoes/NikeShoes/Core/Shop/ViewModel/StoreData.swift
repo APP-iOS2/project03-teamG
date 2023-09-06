84d206d6fe24ac26ba73fa953e07f840cbef6c75
Jiyoung Ahn <wl950204@naver.com>
2023-09-06 10:49:35 +0900

    feat: 시간대에 따라서 영업중/영업종료 나오게 하기

--- NikeShoes/NikeShoes/NikeShoes/Core/Shop/ViewModel/StoreData.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/Shop/ViewModel/StoreData.swift
@@ -22,17 +22,28 @@ struct StoreData: Identifiable, ImageURL {
     var openingTime: String
     var terminatedTime: String
     var imageURLString: String
+    var now: Date
     
     var imageURL: URL {
         URL(string: imageURLString)!
+    }
+    
+    var operationTime: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_kr")
+        dateFormatter.timeZone = TimeZone(abbreviation: "KST")
+        dateFormatter.dateFormat = "a HH시 mm분"
+        dateFormatter.amSymbol = "오전"
+        dateFormatter.pmSymbol = "오후"
+        return dateFormatter.string(from: now)
     }
 }
 
 // 뷰모델 파일 나누기
 class StoreModel: ObservableObject {
     @Published var stores: [StoreData] = [
-        StoreData(name: "나이키 롯데 동탄", district: "경기도", city: "화성시", detailAddress: "동탄역로 160 롯데백화점 동탄점 5층", locationCoordinates: [37.20074, 127.09805], storePhoneNumber: "+82 31 8036 3871", openingTime: "오전 10시 30분", terminatedTime: "오후 8시", imageURLString: "https://static.nike.com/a/images/t_default/2e8d9338-b43d-4ef5-96e1-7fdcfd838f8e/image.jpg"),
-        StoreData(name: "나이키 유나이트 용인", district: "경기도", city: "용인시", detailAddress: "기흥구 신고매로124 롯데프리미엄아울렛", locationCoordinates: [37.22518, 127.12067], storePhoneNumber: "+82 31 8036 3029", openingTime: "오전 10시 30분", terminatedTime: "오후 9시", imageURLString: "https://static.nike.com/a/images/f_auto/428e1194-21f3-434c-9730-40fd79ec602f/image.jpg"),
+        StoreData(name: "나이키 롯데 동탄", district: "경기도", city: "화성시", detailAddress: "동탄역로 160 롯데백화점 동탄점 5층", locationCoordinates: [37.20074, 127.09805], storePhoneNumber: "+82 31 8036 3871", openingTime: "오전 10시 30분", terminatedTime: "오후 8시", imageURLString: "https://static.nike.com/a/images/t_default/2e8d9338-b43d-4ef5-96e1-7fdcfd838f8e/image.jpg", now: Date()),
+        StoreData(name: "나이키 유나이트 용인", district: "경기도", city: "용인시", detailAddress: "기흥구 신고매로124 롯데프리미엄아울렛", locationCoordinates: [37.22518, 127.12067], storePhoneNumber: "+82 31 8036 3029", openingTime: "오전 10시 30분", terminatedTime: "오후 9시", imageURLString: "https://static.nike.com/a/images/f_auto/428e1194-21f3-434c-9730-40fd79ec602f/image.jpg", now: Date()),
     
     ]
 }
