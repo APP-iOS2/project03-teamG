6c5a7f4e2fb759aadd0d175b33e0bf6a8fbebffb
YeoSungeun <dutjddms@naver.com>
2023-09-06 23:24:06 +0900

    refactor: ShopView 스크롤 수정, TabView 배경색 수정

--- NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/RecentItemView.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/RecentItemView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct RecentItemView: View {
     
     @State var sampleRecentItems: [String] = ["에어 조던 1 미드", "나이키 덩크 로우","3","4","5","6","7","8","9","10"]
+    @State var tempItems: [String] = []
     
     var body: some View {
         VStack(alignment: .leading){
             if !sampleRecentItems.isEmpty {
+                
                 HStack(alignment: .bottom) {
                     Text("최근 본 제품")
                         .font(Font.medium20)
@@ -36,27 +38,53 @@ struct RecentItemView: View {
                 ScrollView(.horizontal) {
                     LazyHStack {
                         
-                        
-                        ForEach(sampleRecentItems, id: \.self) { item in
-                            
-                            NavigationLink(destination: ProductDetailView()) { // ItemListView로 이동
+                        // 최근 본 제품 개수 제한 (이거 말고 다른 방법도 있을까?
+                        if sampleRecentItems.count < 6 {
+                            ForEach(sampleRecentItems, id: \.self) { item in
                                 
-                                VStack(alignment: .leading){
+                                NavigationLink(destination: ProductDetailView()) { // ItemListView로 이동
                                     
-                                    AsyncImage(url: URL(string: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/af53d53d-561f-450a-a483-70a7ceee380f/%EB%8D%A9%ED%81%AC-%EB%A1%9C%EC%9A%B0-%EC%97%AC%EC%84%B1-%EC%8B%A0%EB%B0%9C-ZuZyA5Sj.png")) { image in
-                                        image.resizable()
-                                    } placeholder: {
-                                        ProgressView()
+                                    VStack(alignment: .leading){
+                                        
+                                        AsyncImage(url: URL(string: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/af53d53d-561f-450a-a483-70a7ceee380f/%EB%8D%A9%ED%81%AC-%EB%A1%9C%EC%9A%B0-%EC%97%AC%EC%84%B1-%EC%8B%A0%EB%B0%9C-ZuZyA5Sj.png")) { image in
+                                            image.resizable()
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 150, height: 150)
+                                        .clipped()
+                                        
+                                        Text("\(item)")
+                                            .foregroundColor(.black)
+                                            .font(Font.semiBold12)
+                                            .padding([.top,.leading],10)
+                                        
                                     }
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: 150, height: 150)
-                                    .clipped()
+                                }
+                            }
+                        } else {
+                            ForEach(sampleRecentItems[0...4], id: \.self) { item in
+                                
+                                NavigationLink(destination: ProductDetailView()) { // ItemListView로 이동
                                     
-                                    Text("\(item)")
-                                        .foregroundColor(.black)
-                                        .font(Font.semiBold12)
-                                        .padding([.top,.leading],10)
-                                    
+                                    VStack(alignment: .leading){
+                                        
+                                        AsyncImage(url: URL(string: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/af53d53d-561f-450a-a483-70a7ceee380f/%EB%8D%A9%ED%81%AC-%EB%A1%9C%EC%9A%B0-%EC%97%AC%EC%84%B1-%EC%8B%A0%EB%B0%9C-ZuZyA5Sj.png")) { image in
+                                            image.resizable()
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 150, height: 150)
+                                        .clipped()
+                                        
+                                        Text("\(item)")
+                                            .foregroundColor(.black)
+                                            .font(Font.semiBold12)
+                                            .padding([.top,.leading],10)
+                                        
+                                    }
                                 }
                             }
                         }
