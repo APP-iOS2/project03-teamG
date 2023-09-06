fce6d3b4c6c8a4863300fffd7848a7ac4796419c
YeoSungeun <dutjddms@naver.com>
2023-09-06 13:56:55 +0900

    design: InterstItemView, RecommedStoreView 구현

--- NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/RecommendStoreView.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/RecommendStoreView.swift
@@ -8,8 +8,68 @@
 import SwiftUI
 
 struct RecommendStoreView: View {
+    
+    @ObservedObject var storeModel = StoreModel()
+    
+    var sampleStore: [String] = ["여기매장", "저기매장", "저어어기 매장", "저어어어기 매장"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            
+            HStack(alignment: .bottom) {
+                Text("근처 매장")
+                    .font(Font.medium20)
+                Spacer()
+                NavigationLink {
+                    //마이페이지에 팔로잉 수정으로 이동
+                    StoreView()
+                } label: {
+                    Text("매장 찾기")
+                        .font(.system(size: 16))
+                        .fontWeight(.regular)
+                        .foregroundColor(.textGray)
+                }
+                
+            }
+            .padding(1)
+            .padding([.leading,.trailing])
+            
+            //가로 카테고리
+            ScrollView(.horizontal) {
+                LazyHStack {
+                    
+                    
+                    ForEach(storeModel.stores) { store in
+                        
+                        NavigationLink(destination: StoreDetailView(store: store)) {
+                            VStack(alignment: .leading){
+                                
+                                AsyncImage(url: store.imageURL) { image in
+                                    image.resizable()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 255, height: 255)
+                                .clipped()
+                                
+                                Text("\(store.name)")
+                                    .foregroundColor(.black)
+                                    .font(Font.semiBold12)
+                                    .padding([.top,.leading],10)
+                                
+                                
+                                
+                            }
+                        }
+                    }
+                    
+                }
+                .padding()
+            }
+            
+        }
+        .padding([.top,.bottom],20)
     }
 }
 
