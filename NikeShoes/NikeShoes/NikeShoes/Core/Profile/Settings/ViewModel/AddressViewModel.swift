d72405087cc42e9f7d5da04b1b7b22a3adeca777
lhchan4010 <lhchan4010@gmail.com>
2023-09-07 11:43:45 +0900

    feat: MVVM패턴으로 리팩토링

---
+++ NikeShoes/NikeShoes/NikeShoes/Core/Profile/Settings/ViewModel/AddressViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  AddressViewModel.swift
+//  NikeShoes
+//
+//  Created by 이희찬 on 2023/09/07.
+//
+
+import Foundation
+
+class AddressViewModel: ObservableObject {
+    @Published var addresses: [Address] = [sampleAddress]
+    @Published var isChecked: Bool = true
+
+    func toggleCheckmark() {
+        isChecked.toggle()
+    }
+}
+
+let sampleAddress = Address(
+    name: "이희찬",
+    city: "서울특별시",
+    district: "강남구",
+    town: "삼성동",
+    fullAddress: "123-45",
+    postalCode: "06164",
+    phoneNumber: "010-1234-5678",
+    country: "대한민국",
+    isDefault: true
+)
