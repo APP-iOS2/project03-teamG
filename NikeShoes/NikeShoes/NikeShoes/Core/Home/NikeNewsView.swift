805bb21ed2cd4ead11540fccb8b977bb57a34e9a
Chan Ho Park <park_ch@kakao.com>
2023-09-07 17:14:30 +0900

    design: homeView 구현중
    1. 나이키소식 gif 구현

--- NikeShoes/NikeShoes/NikeShoes/Core/Home/NikeNewsView.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/Home/NikeNewsView.swift
@@ -6,53 +6,76 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct NikeNewsView: View {
     var body: some View {
         VStack(alignment: .leading) {
-            HStack(alignment: .bottom) {
-                Text("나이키 소식")
-                    .font(Font.medium20)
-                Spacer()
-                NavigationLink {
-                    // 소식모두보기
-                    Text("소식 모두 보기")
-                } label: {
-                    Text("모두 보기")
-                        .font(.system(size: 16))
-                        .fontWeight(.regular)
-                        .foregroundColor(.textGray)
-                }
-            }
-            .padding(1)
-            .padding([.leading, .trailing])
-            
-            // 소식칸
-            LazyVStack {
-                NavigationLink(destination: Text("해당 페이지로")) {
-                    VStack(alignment: .leading) {
-                        // 상품 이미지
-                        AsyncImage(url: URL(string: "https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/w_896,c_limit/f9e8bd6a-04f6-4db5-8060-55247708bd9d/%EC%A1%B0%EB%8D%98-%EC%8A%A4%ED%83%80%EC%9D%BC%EB%A7%81.jpg")) { image in
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 393, height: 525)
-                                .clipped()
-                        } placeholder: {
-                            Image("progress")
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(height: 180)
-                                .clipped()
-                        }
-                        
-                    }
-                }
-                
-                .padding()
+            // 헤더 부분
+            headerSection
+            // GIF 이미지와 관련 텍스트
+            contentSection
+        }
+    }
+    
+    // 헤더 섹션
+    private var headerSection: some View {
+        HStack(alignment: .bottom) {
+            Text("나이키 소식")
+                .font(.medium20)
+            Spacer()
+            NavigationLink(destination: Text("소식 모두 보기")) {
+                Text("모두 보기")
+                    .font(.system(size: 16))
+                    .fontWeight(.regular)
+                    .foregroundColor(.textGray)
             }
         }
-        .padding([.top, .bottom], 30)
+        .padding(EdgeInsets(top: 1, leading: 16, bottom: 20, trailing: 16))
+    }
+    
+    // 콘텐츠 섹션
+    private var contentSection: some View {
+        ZStack {
+            // 웹에서 GIF 이미지를 로드
+            WebImage(url: URL(string: "https://media.giphy.com/media/3owyoSnOZXam3QgkfK/giphy.gif"))
+                .resizable()
+                .indicator(.activity)
+                .transition(.fade(duration: 0.5))
+                .scaledToFit()
+            
+            // 텍스트와 "자세히 보기" 버튼
+            VStack {
+                Spacer()
+                detailText
+                actionButton
+            }
+        }
+    }
+    
+    // 상세 텍스트
+    private var detailText: some View {
+        Text("JUST DO IT\n빛나는 자신감\n새로운 에너지를 더하다")
+            .bold()
+            .font(.system(size: 40, weight: .heavy))
+            .foregroundColor(.white)
+            .padding(.bottom, -20)  // 텍스트와 버튼 사이의 간격을 줄이기 위해
+    }
+    
+    // 자세히 보기 버튼
+    private var actionButton: some View {
+        HStack {
+            NavigationLink(destination: Text("자세한 내용")) {
+                Text("자세히 보기")
+                    .foregroundColor(.black)
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 8)
+                    .background(Color.white)
+                    .cornerRadius(50)
+            }
+            .padding(20)
+            Spacer()
+        }
     }
 }
 
