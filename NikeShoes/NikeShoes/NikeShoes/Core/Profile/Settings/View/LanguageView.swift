99c9c393b0f55b40a442a9d8acbc7a20df80b0de
lhchan4010 <lhchan4010@gmail.com>
2023-09-07 10:16:16 +0900

    feat: modal custom

--- NikeShoes/NikeShoes/NikeShoes/Core/Profile/Settings/View/LanguageView.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/Profile/Settings/View/LanguageView.swift
@@ -15,58 +15,82 @@ struct LanguageView: View {
     
     let title: String
     let logoURL: URL = URL(string: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fo2k2o%2FbtqwR37aRMc%2FimACexBUQZJms7mQRkQck1%2Fimg.jpg")!
-
+    let Languages = ["한국어", "영어", "스페인어", "프랑스어", "아랍어"]
+    
     var body: some View {
-        VStack(alignment: .leading){
-            Spacer()
-            
-            AsyncImage(url: logoURL) { phase in
-                switch phase {
-                case .empty:
-                    ProgressView()
-                case .success(let image):
-                    image
-                        .resizable()
-                        .frame(width: 144, height: 72)
-                case .failure:
-                    Text("Failed to load image.")
-                @unknown default:
-                    EmptyView()
+        ZStack(){
+            VStack(alignment: .leading){
+                Spacer()
+                AsyncImage(url: logoURL) { phase in
+                    switch phase {
+                    case .empty:
+                        ProgressView()
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .frame(width: 144, height: 72)
+                    case .failure:
+                        Text("Failed to load image.")
+                    @unknown default:
+                        EmptyView()
+                    }
                 }
+                
+                Text("언어 선택")
+                    .font(.title)
+                Text("Nike 앱에서 지원되는 언어를 업데이트하세요")
+                    .font(.subheadline)
+                
+                Button {
+                    showModal.toggle()
+                } label: {
+                    HStack{
+                        Text(selectedLang)
+                            .foregroundColor(.black)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "chevron.down")
+                            .foregroundColor(.black)
+                    }
+                    .padding()
+                }
+                .overlay(RoundedRectangle(cornerRadius: 30).stroke(Color.black, lineWidth: 0.5))
+                .frame(width: 250)
+                .padding(.top)
+                
+                Spacer()
+                
+                CustomButton(background: .black, foregroundColor: .white, title: "확인", action: {
+                    presentationMode.wrappedValue.dismiss()
+                })
             }
             
-            Text("언어 선택")
-                .font(.title)
-            Text("Nike 앱에서 지원되는 언어를 업데이트하세요")
-                .font(.subheadline)
-            
-            Button {
-                showModal.toggle()
-            } label: {
-                HStack{
-                    Text(selectedLang)
-                        .foregroundColor(.black)
-                    
-                    Spacer()
-                    
-                    Image(systemName: "chevron.down")
-                        .foregroundColor(.black)
-                }
-                .padding()
+            if showModal {
+                Color.gray
+                    .opacity(0.3)
+                    .edgesIgnoringSafeArea(.all)
+                    .onTapGesture {
+                        withAnimation {
+                            showModal = false
+                        }
+                    }
             }
-            .overlay(RoundedRectangle(cornerRadius: 30).stroke(Color.black, lineWidth: 0.5))
-            .frame(width: 250)
-            .padding(.top)
-            
-            Spacer()
-            
-            CustomButton(background: .black, foregroundColor: .white, title: "확인", action: {
-                presentationMode.wrappedValue.dismiss()
-            })
-            
-        }
-        .sheet(isPresented: $showModal) {
-            slecteLanguageSheetView(selectedLang: $selectedLang)
+            VStack {
+                Picker("언어선택", selection: $selectedLang) {
+                    ForEach(Languages, id: \.self) { lang in
+                        Text(lang).tag(lang)
+                    }
+                }
+                .pickerStyle(WheelPickerStyle())
+
+            }
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 4)
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(y: showModal ? UIScreen.main.bounds.height / 3 : UIScreen.main.bounds.height)
+            .animation(.easeInOut(duration: 1.0), value: showModal)
         }
         .modifier(NavigationNikeSetting(title: title))
     }
@@ -77,28 +101,5 @@ struct LanguageView_Previews: PreviewProvider {
         NavigationStack{
             LanguageView(title: "언어 선택")
         }
-    }
-}
-
-struct slecteLanguageSheetView: View {
-    @Environment(\.presentationMode) var presentationMode
-    
-    @Binding var selectedLang: String
-    let Languages = ["한국어", "영어", "스페인어", "프랑스어", "아랍어"]
-    
-    var body: some View {
-        VStack(spacing: 20) {
-            Picker("Select a Language", selection: $selectedLang) {
-                ForEach(Languages, id: \.self) { Lang in
-                    Text(Lang).tag(Lang)
-                }
-            }
-            .pickerStyle(WheelPickerStyle())
-            
-            CustomButton(background: .black, foregroundColor: .white, title: "완료", action: {
-                presentationMode.wrappedValue.dismiss()
-            })
-        }
-        .padding()
     }
 }
