7d8e4aeda9acdcfb35b902b7e6b2ee93ae8ad072
a-jb97 <a_jb97@naver.com>
2023-09-10 22:49:19 +0900

    refactor: 코드 리팩토링

---
+++ NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/ProductInfo/CartButtonView.swift
@@ -0,0 +1,35 @@
+//
+//  CartButtonView.swift
+//  NikeShoes
+//
+//  Created by 전민돌 on 9/10/23.
+//
+
+import SwiftUI
+
+struct CartButtonView: View {
+    var body: some View {
+        Button {
+            print("장바구니에 추가")
+        } label: {
+            ZStack {
+                Rectangle()
+                    .stroke(Color.black, lineWidth: 1)
+                    .frame(width: 351, height: 63)
+                    .background(.black)
+                    .cornerRadius(31.5)
+                
+                Text("장바구니")
+                    .foregroundColor(.white)
+                    .font(.system(size: 18))
+                    .fontWeight(.medium)
+            }
+        }
+    }
+}
+
+struct CartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CartButtonView()
+    }
+}
---
+++ NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/ProductInfo/SizeButton/SizeButtonView.swift
@@ -0,0 +1,40 @@
+//
+//  SizeButtonView.swift
+//  NikeShoes
+//
+//  Created by 전민돌 on 9/10/23.
+//
+
+import SwiftUI
+
+struct SizeButtonView: View {
+    @State private var isShowingSizeSheet = false
+    
+    var body: some View {
+        Button {
+            isShowingSizeSheet.toggle()
+        } label: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 31.5)
+                    .stroke(Color.defaultGray, lineWidth: 1)
+                    .frame(width: 351, height: 63)
+                    .background(.white)
+                
+                Text("사이즈 선택  \(Image(systemName: "chevron.down"))")
+                    .foregroundColor(.black)
+                    .font(.system(size: 18))
+                    .fontWeight(.medium)
+            }
+        }
+        .sheet(isPresented: $isShowingSizeSheet) {
+            SizeView()
+                .presentationDetents([.medium, .medium])
+        }
+    }
+}
+
+struct SizeButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        SizeButtonView()
+    }
+}
