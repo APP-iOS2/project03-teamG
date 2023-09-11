9adc2ee0f18facb0eccde0ca9334943e139fe634
Chan Ho Park <park_ch@kakao.com>
2023-09-11 09:54:10 +0900

    design: homeView 구현
    1. 나이키소식 gif 구현

--- NikeShoes/NikeShoes/NikeShoes/Core/Home/NikeNewsView.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/Home/NikeNewsView.swift
@@ -1,4 +1,3 @@
-//
 //  NikeNewsView.swift
 //  NikeShoes
 //
@@ -11,55 +10,52 @@ import SDWebImageSwiftUI
 struct NikeNewsView: View {
     var body: some View {
         VStack(alignment: .leading) {
-
             HStack(alignment: .bottom) {
                 Text("나이키 소식")
-                    .font(Font.medium20)
+                    .font(.medium20)
                 Spacer()
-                NavigationLink {
-                    // 소식모두보기
-                    Text("소식 모두 보기")
-                } label: {
+                NavigationLink(destination: Text("소식 모두 보기")) {
                     Text("모두 보기")
                         .font(.system(size: 16))
                         .fontWeight(.regular)
                         .foregroundColor(.textGray)
                 }
             }
-            .padding(1)
-            .padding([.leading, .trailing])
-            
-            // 소식칸
-            LazyVStack {
-                NavigationLink(destination: Text("해당 페이지로")) {
-                    VStack(alignment: .leading) {
-                        // 상품 이미지
-                        AsyncImage(url: URL(string: "https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/w_896,c_limit/f9e8bd6a-04f6-4db5-8060-55247708bd9d/%EC%A1%B0%EB%8D%98-%EC%8A%A4%ED%83%80%EC%9D%BC%EB%A7%81.jpg")) { image in
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 393, height: 525)
-                                .clipped()
-                        } placeholder: {
-                            Image("progress")
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(height: 180)
-                                .clipped()
+            .padding(EdgeInsets(top: 1, leading: 16, bottom: 20, trailing: 16))
+
+            ZStack {
+                WebImage(url: URL(string: "https://media.giphy.com/media/3owyoSnOZXam3QgkfK/giphy.gif"))
+                    .resizable()
+                    .indicator(.activity)
+                    .transition(.fade(duration: 0.5))
+                    .scaledToFit()
+
+                VStack {
+                    Spacer()
+
+                    Text("JUST DO IT\n빛나는 자신감\n새로운 에너지를 더하다")
+                        .bold()
+                        .font(.system(size: 40, weight: .heavy))
+                        .foregroundColor(.white)
+//                        .multilineTextAlignment(.center)
+                        .padding(.bottom, -20)  // 이 부분을 추가하여 텍스트와 버튼 사이의 간격을 줄입니다.
+
+                    HStack {
+                        NavigationLink(destination: Text("자세한 내용")) {
+                            Text("자세히 보기")
+                                .foregroundColor(.black)
+                                .padding(.horizontal, 16)
+                                .padding(.vertical, 8)
+                                .background(Color.white)
+                                .cornerRadius(50)
                         }
-                        
+                        .padding(20)
+
+                        Spacer()
                     }
                 }
-                
-                .padding()
-
             }
-            .padding(20)
-            Spacer()
         }
-
-        .padding([.top, .bottom], 30)
-
     }
 }
 
