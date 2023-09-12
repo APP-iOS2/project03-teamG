bb4da6080ccb692c89a36d9b13636a4a0b4de353
Rudy-009 <rofxnaos@gmail.com>
2023-09-12 17:07:56 +0900

    feat: SearchResultViewModel 추가 및 Filter기능 구현

--- NikeShoes/NikeShoes/NikeShoes/Core/Shop/ViewModel/SearchResultViewModel.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/Shop/ViewModel/SearchResultViewModel.swift
@@ -11,29 +11,24 @@ import NikeShoesCore
 class SearchResultViewModel {
     
     let service: FirestoreService
+    let searchKeyword: String
     
     @Published var shoes: [ShoesDTO] = []
     
-    // Todo: db path 정의해서 만들기
-    //
-    // Todo: Service 쪽 generic으로 만들기
-
-    // Todo: ViewModel 만들기
-    //
-    // Todo: ViewModel -> View로 데이터 바인딩
-    //
-    // Todo: 전체 앱에서 공유할 객체 정하기?
-    
-    init(service: FirestoreService = DefaultFireStoreService(), search: String) {
+    init(service: FirestoreService = DefaultFireStoreService(), searchKeyword: String) {
         self.service = service
-        action()
-        
+        self.searchKeyword = searchKeyword
+        action()//fetch 후 배열에 반영
     }
     
     func action() {
         Task {
             let shoes = try await fetchItems()
+            print("\(shoes)")
             self.shoes = shoes
+            let filtered = try await filtered(search: searchKeyword)
+            self.shoes = filtered
+            print("Filtered \(self.shoes)")
         }
     }
     
@@ -43,14 +38,27 @@ class SearchResultViewModel {
         return values
     }
     
-    func filtered(search: String) {
+    func filtered(search: String) async throws -> [ShoesDTO] {
         //검색어에 해당하는 신발만이 배열에 들어가야 함
         //or 검색어에 해당하지 않으면 빠져야 함
         
-        //인스턴스의 내용 중 하나라도 검생어에 해당이 되면 remove 안하기?
-        
-        //
-        
+        //이름, 카테고리, 모델이름 기반으로 검색되게 만듬
+        let filteredShoes = self.shoes.filter {
+            $0.name.contains(search)
+            || $0.category.contains(search)
+            || $0.modelName.contains(search)
+        }
+        return filteredShoes
     }
     
+//    public var name: String
+//    public var category: String   // Gender : 남성, 여성, 키즈 ,(공용)
+//    public var modelName: String //  조던 에어 덩크 코르
+//    public var description: String
+//    public var price: Int
+//    public var size: [Int]
+//    public var colors: [ProductColor]
+//    public var imageURLString: [String]
+//    public var speciality: [Speciality]
+//    public var stylingImage: [String]
 }
