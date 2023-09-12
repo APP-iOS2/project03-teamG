a7833a213fcd0e9abfb5557d9aab89d55ce657b6
lhchan4010 <lhchan4010@gmail.com>
2023-09-12 12:37:39 +0900

    fix: back

--- NikeShoes/NikeShoes/NikeShoes/Core/Profile/Settings/ViewModel/AddressViewModel.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/Profile/Settings/ViewModel/AddressViewModel.swift
@@ -14,39 +14,40 @@ class AddressViewModel: ObservableObject {
     
     private var db = Firestore.firestore()
     
-    func addAddress(address: AddressDTO) async {
+    func addAddress(address: AddressDTO) {
         do {
             if address.isDefault {
                 if let currentDefault = addresses.first(where: { $0.isDefault }) {
                     var updatedAddress = currentDefault
                     updatedAddress.isDefault = false
-                    try await updateAddress(address: updatedAddress)
+                    try db.collection("addresses").document(currentDefault.id ?? "").setData(from: updatedAddress)
                 }
             }
-            let _ = try db.collection("user").document(Auth.auth().currentUser!.uid)
-                .collection("addresses").addDocument(from: address)
+            let _ = try db.collection("user").document(Auth.auth().currentUser!.uid) .collection("addresses").addDocument(from: address)
         } catch {
             print(error.localizedDescription)
         }
     }
     
-    func fetchData() async {
+    func fetchData() {
         guard let userID = Auth.auth().currentUser?.uid else {
             print("No user ID available")
             return
         }
         
-        do {
-            let querySnapshot = try await db.collection("user").document(userID).collection("addresses").getDocuments()
-            self.addresses = querySnapshot.documents.compactMap { queryDocumentSnapshot in
+        db.collection("user").document(userID).collection("addresses").addSnapshotListener { (querySnapshot, error) in
+            guard let documents = querySnapshot?.documents else {
+                print("No documents")
+                return
+            }
+            
+            self.addresses = documents.compactMap { queryDocumentSnapshot in
                 try? queryDocumentSnapshot.data(as: AddressDTO.self)
             }
-        } catch {
-            print("Failed to fetch data: \(error)")
         }
     }
     
-    func updateAddress(address: AddressDTO) async throws {
+    func updateAddress(address: AddressDTO) {
         guard let addressID = address.id else {
             print("No address ID available")
             return
@@ -58,24 +59,21 @@ class AddressViewModel: ObservableObject {
                 .setData(from: address)
         } catch let error {
             print("Error updating address: \(error)")
-            throw error
         }
     }
     
-    func setAsDefault(address: AddressDTO) async {
-        do {
-            if address.isDefault {
-                if let currentDefault = addresses.first(where: { $0.isDefault }) {
-                    var updatedAddress = currentDefault
-                    updatedAddress.isDefault = false
-                    try await updateAddress(address: updatedAddress)
-                }
+    func setAsDefault(address: AddressDTO) {
+        if address.isDefault {
+            // 기존 기본 배송지 해제
+            if let currentDefault = addresses.first(where: { $0.isDefault }) {
+                var updatedAddress = currentDefault
+                updatedAddress.isDefault = false
+                updateAddress(address: updatedAddress)
             }
-            
-            try await updateAddress(address: address)
-        } catch {
-            print("Failed to set as default: \(error)")
         }
+        
+        // 새로운 기본 배송지 설정
+        updateAddress(address: address)
     }
+    
 }
-
