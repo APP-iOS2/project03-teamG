863db33eb57dfb22017186126bdf65495539d5ee
ahshotchoo <wjdchl@ewhain.net>
2023-09-12 10:03:51 +0900

    feat: UserDTO 수정

--- NikeShoes/NikeShoesCore/Sources/NikeShoesCore/Service/DTO/UserDTO.swift
+++ NikeShoes/NikeShoesCore/Sources/NikeShoesCore/Service/DTO/UserDTO.swift
@@ -10,13 +10,15 @@ import FirebaseFirestoreSwift
 
 public struct UserDTO: Codable {
     @DocumentID public var id: String?
+    public var firstName: String
+    public var lastName: String
     public var email: String
     public var phoneNumber: String
     public var dateOfBirth: String
     public var memberReward: String // 유저한테 주는 프로모션
-    public var name: String
-    public var gender: [Gender] // 유저 팔로잉이랑 연결?
     public var address: [AddressDTO]
-    public var interest: [String]
+    public var following: [Following] // 관심사
     public var size: [Int]
+    public var activityArea : String //활동 지역(프로필 수정 뷰)
+    public var introContent: String //자기소개(프로필 수정 뷰)
 }
