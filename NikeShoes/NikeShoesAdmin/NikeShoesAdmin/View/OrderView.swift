d6ca13989ec4a7838a860f10f0ba0d3282bcda4c
YeoSungeun <dutjddms@naver.com>
2023-09-13 14:54:02 +0900

    feat: OrderView 파이어베이스연결

--- NikeShoes/NikeShoesAdmin/NikeShoesAdmin/View/OrderView.swift
+++ NikeShoes/NikeShoesAdmin/NikeShoesAdmin/View/OrderView.swift
@@ -12,7 +12,8 @@ struct OrderView: View {
     
     @ObservedObject var orderViewModel: OrderViewModel = OrderViewModel()
     @State var ischeckedPayment: Bool = false
-    @State var deliveryCheck: String = "배송 전"
+    @State var deliveryStatus: String = "주문 상태"
+    @State private var selectedStatus = DeliveryStatus.orderComplete
     
     var body: some View {
         List {
@@ -21,61 +22,63 @@ struct OrderView: View {
                     Text("주문 번호: \(order.id ?? "")")
                         .font(.caption)
                         .foregroundColor(.gray)
-                    
                     Spacer()
+                
+                    Menu {
+                        //
+                        Button(DeliveryStatus.orderComplete.rawValue) {
+                            deliveryStatus = "주문 완료"
+                            Task { try await orderViewModel.updateOrder(id: order.id ?? "K2n0ycQzHc4ghgT0sy8q", deliveryStatus: DeliveryStatus.orderComplete.rawValue)
+                            }
+                            
+                        }
+                        Button(DeliveryStatus.paymentComfirm.rawValue) {
+                            deliveryStatus = "입금 확인"
+                            Task { try await orderViewModel.updateOrder(id: order.id ?? "K2n0ycQzHc4ghgT0sy8q", deliveryStatus: DeliveryStatus.paymentComfirm.rawValue)
+                            }
+                        }
+                        Button(DeliveryStatus.shipping.rawValue) {
+                            Task { try await orderViewModel.updateOrder(id: order.id ?? "K2n0ycQzHc4ghgT0sy8q", deliveryStatus: DeliveryStatus.shipping.rawValue)
+                            }
+                        }
+                        Button(DeliveryStatus.deliveryComplete.rawValue) {
+                            Task { try await orderViewModel.updateOrder(id: order.id ?? "K2n0ycQzHc4ghgT0sy8q", deliveryStatus: DeliveryStatus.deliveryComplete.rawValue)
+                            }
+                        }
+                        
+                    } label: {
+                        Text("\(order.deliveryStatus.rawValue)")
+                            .fontWeight(.semibold)
+                    }
+                    .buttonStyle(.borderedProminent)
                     
-                    // 데이터 연결 후 값 바꿔줄 수 있게 변경
-                    //                   if order.paymentStatus {
-                    //                       Text("입금 완료")
-                    //                   } else {
-                    //                       Button {
-                    //                           //order.paymentComplete 값 바꿔주기
-                    //                       } label: {
-                    //                           Text("입금 확인 중")
-                    //                       }
-                    //                   }
+                    
+                    
                     
                 }
                 HStack {
                     VStack(alignment: .leading) {
                         Group {
-                            Text("\(order.orderDate)")
+                            Text("\(orderViewModel.toString(orderDate: order.orderDate))")
                             Text("주문자: \(order.userID)")
                             Text("제품코드: \(order.shoesID)")
                             Text("배송지: \(order.address)")
                         }
                         .padding(.bottom, 1)
                     }
-                    
-                    Spacer()
-                    
-                    //데이터 연결 전에 임의 버튼들
-                    // TODO: 여기 이넘으로 바꾸기~~~
-                    Menu {
-                        //
-                        Button("어쩌꾸") {
-                            deliveryCheck = "어쩌구 중"
-                        }
-                        Button("발송 완료?") {
-                            deliveryCheck = "발송 완료"
-                        }
-                    } label: {
-                        Text("\(deliveryCheck)")
-                            .buttonStyle(.bordered)
-                            .background(Color.white)
-                    }
-                    
-                    
+            
                 }
             }
-            
-            
+        }
+        // TODO: 이거 적용 안되는듯
+        .refreshable {
+            Task { try await orderViewModel.fetchOrder()
+            }
         }
         .toolbar {
             ToolbarItem(placement: .navigationBarTrailing) {
                 Button {
-                    
-                    let orderDTO: OrderDTO = OrderDTO(id: "wnans1qjs", shoesID: "AJ4v0y1e9hFgYfUcel9T", userID: "HIKeuU6XTsQehBASib1M49UoWen2", address: "서울시 강서구 어디로 4", deliveryStatus: .orderComplete, orderDate: Date())
+                    let orderDTO: OrderDTO = OrderDTO(id: "wnans1qjs", shoesID: "AJ4v0y1e9hFgYfUcel9T", userID: "HIKeuU6XTsQehBASib1M49UoWen2", address: "성훈님 개발천재짱짱짱!!!! ", deliveryStatus: .orderComplete, orderDate: Date())
                     
                     Task { try await orderViewModel.createOrder(orderDTO: orderDTO)
                     }
