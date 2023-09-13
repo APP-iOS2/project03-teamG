d615d14e3f8425967c8d75b7a54d4d28fe502999
pHH <gudghks56@gmail.com>
2023-09-13 09:24:58 +0900

    feat: OB ViewModel 추가

---
+++ NikeShoes/NikeShoes/NikeShoes/Core/OnBoarding/VIewModel/OBViewModel.swift
@@ -0,0 +1,59 @@
+//
+//  OBViewModel.swift
+//  NikeShoes
+//
+//  Created by 박형환 on 2023/09/12.
+//
+
+import Foundation
+import NikeShoesCore
+import FirebaseAuth
+
+final class OBViewModel: ObservableObject {
+    
+    private let service: FirestoreService
+    
+    init(service: FirestoreService) {
+        self.service = service
+    }
+    
+    func updateLocation() async {
+        
+    }
+    
+    func updateMyShoesSize(sizes: [Int]) async {
+        guard let user_uid = Auth.auth().currentUser?.uid else { return }
+        do {
+            try await service.update(collection: .user, document: user_uid, fields: ["size": sizes])
+        } catch {
+            print("updateLocation : \(error)")
+        }
+    }
+    
+    func updateInterest(interest: [Following]) async {
+        guard let user_uid = Auth.auth().currentUser?.uid else { return }
+        
+        let interestDTO = interest.map { FollowingDTO(title: $0.title, imageURL: $0.imageURL) }
+        
+        do {
+            for dto in interest {
+                let result = try await service.create(send: dto,
+                                                      collection: .user,
+                                                      document: user_uid,
+                                                      collection2: .user_following)
+            }
+        } catch {
+            
+        }
+    }
+    
+    func updateLanguage() async {
+//        guard let user_uid = Auth.auth().currentUser?.uid else { return }
+//        do {
+//            try await service.update(collection: .user, document: user_uid, fields: ["country": "한국"])
+//        } catch {
+//            print("updateLocation : \(error)")
+//        }
+    }
+    
+}
