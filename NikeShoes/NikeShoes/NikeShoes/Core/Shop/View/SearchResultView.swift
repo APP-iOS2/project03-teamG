4ec811c3d89bb8b26eb4d659d1bfd085f6c5d3b3
Rudy-009 <rofxnaos@gmail.com>
2023-09-13 17:12:45 +0900

    feat: 검색결과 없을 시 결과없음 페이지 추가

--- NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/SearchResultView.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/SearchResultView.swift
@@ -35,6 +35,9 @@ struct SearchResultView: View {
     // 뷰 본문
     var body: some View {
         //NavigationStack {
+        if searchResultViewModel.shoes.isEmpty {
+            Text("검색 결과가 존재하지 않습니다.")
+        } else {
             ScrollView {
                 
                 // 상품 목록을 그리드로 표시
@@ -92,28 +95,29 @@ struct SearchResultView: View {
                     }
                 }
                 .padding()
-            //}
-            .navigationTitle("앱 전용 제품")
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationBarBackButtonHidden(true)
-            
-            // 네비게이션 바 아이템 설정
-            .navigationBarItems(
-                leading: Button(action: {
-                    self.presentationMode.wrappedValue.dismiss()
-                }) {
-                    Image(systemName: "chevron.left")
-                        .foregroundColor(.black)
-                },
-                // 검색 아이콘을 네비게이션 바 오른쪽에 배치
-                trailing: NavigationLink(destination: SearchItemView()) {
-                    Image(systemName: "magnifyingglass")
-                        .foregroundColor(.black)
-                }
-            )
-        }
-        .onAppear {
-            print("SRV Called")
+                //}
+                .navigationTitle("앱 전용 제품")
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarBackButtonHidden(true)
+                
+                // 네비게이션 바 아이템 설정
+                .navigationBarItems(
+                    leading: Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Image(systemName: "chevron.left")
+                            .foregroundColor(.black)
+                    },
+                    // 검색 아이콘을 네비게이션 바 오른쪽에 배치
+                    trailing: NavigationLink(destination: SearchItemView()) {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.black)
+                    }
+                )
+            }
+            .onAppear {
+                print("SRV Called")
+            }
         }
     }
 }
