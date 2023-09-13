1a8232c1b9a51d45aa948866ceee88d9fc891572
Chan Ho Park <park_ch@kakao.com>
2023-09-13 16:43:49 +0900

    feat: 나이키 앱 베스트 컬렉션 구현
    전체적인 나이키 앱 베스트 컬렉션 구현
    
    세부 추가 구현예정 :
    1. 큰 카테고리인 남성, 여성, 키즈 별로 세분화 작업 필요
    2. 카테고리 도 동일한 방식으로 작업 예정

--- NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/AppBestCollectionView.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/AppBestCollectionView.swift
@@ -6,28 +6,41 @@
 //
 
 import SwiftUI
+import NikeShoesCore
+
+// 컬렉션 정보를 담을 구조체
+struct CollectionInfo {
+    let title: String
+    let imageURL: String
+    let speciality: Speciality?
+    let navigationTitle: String
+}
 
 struct AppBestCollectionView: View {
-
-    var sampleCollections: [String] = ["앱 전용 제품", "신제품", "이번 주 베스트", "러닝화 가이드", "전체 제품 보기"]
+    // 샘플 컬렉션 데이터
+    var sampleCollections: [CollectionInfo] = [
+        CollectionInfo(title: "앱 전용 제품", imageURL: "https://static.nike.com/a/images/f_auto/dpr_1.7,cs_srgb/w_827,c_limit/0a286516-b1a3-4a3d-975f-170b7b8a7a50/nike-just-do-it.jpg", speciality: .onlyApp, navigationTitle: "앱 전용 제품"),
+        CollectionInfo(title: "신제품", imageURL: "https://static.nike.com/a/images/f_auto/dpr_1.7,cs_srgb/w_827,c_limit/de43c365-10d2-41c9-89ba-7ed7cbcdb817/nike-just-do-it.jpg", speciality: .newProduct, navigationTitle: "신제품"),
+        CollectionInfo(title: "이번 주 베스트", imageURL: "https://static.nike.com/a/images/f_auto/dpr_1.7,cs_srgb/w_827,c_limit/30dc6933-29ef-4973-a3f5-947e9f23a122/nike-just-do-it.jpg", speciality: .hot, navigationTitle: "이번 주 베스트"),
+        CollectionInfo(title: "전체 제품 보기", imageURL: "https://static.nike.com/a/images/w_960,c_limit,f_auto/e2330902-f398-4559-95c1-405cdef9fc66/get-%E2%80%98em-while-you-can.jpg", speciality: nil, navigationTitle: "전체 제품 보기")
+    ]
 
     var body: some View {
         VStack(alignment: .leading) {
-            
+            // 제목
             Text("나이키 앱 베스트 컬렉션")
-                .font(Font.medium20)
+                .font(.headline)
                 .padding(.leading, 20)
             
-            // 가로 카테고리
+            // 가로 스크롤 뷰
             ScrollView(.horizontal) {
                 LazyHStack {
-
-                    ForEach(sampleCollections, id: \.self) { collection in
-
-                        NavigationLink(destination: ItemListView()) { // ItemListView로 이동
-
+                    // 각 컬렉션에 대한 정보를 표시
+                    ForEach(sampleCollections, id: \.title) { collection in
+                        NavigationLink(destination: ItemListViewWithProgressbar(speciality: collection.speciality, navigationTitle: collection.title)) {
                             VStack(alignment: .leading) {
-                                AsyncImage(url: URL(string: "https://static.nike.com/a/images/w_1920,c_limit/wzrqsjqpg6pslrevtkpl/image.png")) { image in
+                                // 이미지 로딩
+                                AsyncImage(url: URL(string: collection.imageURL)) { image in
                                     image
                                         .resizable()
                                         .aspectRatio(contentMode: .fill)
@@ -36,10 +49,11 @@ struct AppBestCollectionView: View {
                                 }
                                 .frame(width: 150, height: 150)
                                 .cornerRadius(15)
-
-                                Text("\(collection)")
+                                
+                                // 컬렉션 제목
+                                Text("\(collection.title)")
                                     .foregroundColor(.black)
-                                    .font(Font.semiBold12)
+                                    .font(.subheadline)
                                     .padding([.top, .leading], 10)
                             }
                         }
@@ -54,7 +68,7 @@ struct AppBestCollectionView: View {
 
 struct AppBestCollectionView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView { // 별도의 NavigationView로 미리보기 설정
+        NavigationView {
             AppBestCollectionView()
                 .frame(height: 280)
         }
