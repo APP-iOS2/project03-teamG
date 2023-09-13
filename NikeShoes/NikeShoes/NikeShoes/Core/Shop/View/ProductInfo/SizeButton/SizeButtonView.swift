9227b44c02e7e93832df20d2babeacaded1521ca
ahshotchoo <wjdchl@ewhain.net>
2023-09-13 23:30:24 +0900

    feat: SizeView 선택한 사이즈 값 받아옴

--- NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/ProductInfo/SizeButton/SizeButtonView.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/Shop/View/ProductInfo/SizeButton/SizeButtonView.swift
@@ -10,6 +10,8 @@ import NikeShoesCore
 
 struct SizeButtonView: View {
     @State private var isShowingSizeSheet = false
+    @State var isSizeSelected = false
+    @State var selectedSize = 240
     var shoesData: ShoesDTO
     
     var body: some View {
@@ -22,14 +24,21 @@ struct SizeButtonView: View {
                     .frame(width: 351, height: 63)
                     .background(.white)
                 
-                Text("사이즈 선택  \(Image(systemName: "chevron.down"))")
-                    .foregroundColor(.black)
-                    .font(.system(size: 18))
-                    .fontWeight(.medium)
+                if isSizeSelected {
+                    Text("\(selectedSize) 사이즈  \(Image(systemName: "chevron.down"))")
+                        .foregroundColor(.black)
+                        .font(.system(size: 18))
+                        .fontWeight(.medium)
+                } else {
+                    Text("사이즈 선택  \(Image(systemName: "chevron.down"))")
+                        .foregroundColor(.black)
+                        .font(.system(size: 18))
+                        .fontWeight(.medium)
+                }
             }
         }
         .sheet(isPresented: $isShowingSizeSheet) {
-            SizeView(shoesData: shoesData)
+            SizeView(isSizeSelected: $isSizeSelected, selectedSize: $selectedSize, shoesData: shoesData)
                 .presentationDetents([.medium, .medium])
         }
     }
