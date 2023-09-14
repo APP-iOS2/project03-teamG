4a57c34302640c4b4007092d168e3e091d9ca9c6
Seolah Chloe Chung <seolahchloe.chung@gmail.com>
2023-09-14 15:01:00 +0900

    fix: buyView 충돌 에러 해결

--- NikeShoes/NikeShoes/NikeShoes/Core/Bag/PaymentView.swift
+++ NikeShoes/NikeShoes/NikeShoes/Core/Bag/PaymentView.swift
@@ -11,6 +11,8 @@ import Firebase
 import FirebaseAuth
 
 struct PaymentView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
     @State var selectedQty: Int
     @State var finalPrice: String
     
@@ -38,6 +40,8 @@ struct PaymentView: View {
     @EnvironmentObject var orderViewModel: OrderViewModel
     @EnvironmentObject var authViewModel: AuthViewModel
     
+    @State private var isAgreeTapped: Bool = false
+    @State private var isAccountTransferViewPresented: Bool = false
     
     var body: some View {
         
@@ -355,7 +359,7 @@ struct PaymentView: View {
                                 }
                                 
                                 Button {
-//                                        isArgreeTapped.toggle()
+                                        isArgreeTapped.toggle()
                                     authViewModel.fetchUser()
                                     let orderList: [OrderDTO] = [
                                         OrderDTO(shoesID: "\(bagItemList[0].id)" ,
@@ -371,6 +375,10 @@ struct PaymentView: View {
                                         }
                                     }
                                     
+                                    isAccountTransferViewPresented = true
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                                        presentationMode.wrappedValue.dismiss()
+                                    }
                                 } label: {
                                     Text("결제하기")
                                         .font(.title3)
@@ -382,7 +390,18 @@ struct PaymentView: View {
                                         .cornerRadius(40)
                                         .padding()
                                 }
-                            }.padding()
+                                .frame(width: 330, height: 60)
+                                       .background(isAgreeTapped ? Color.gray.opacity(0.3) : Color.black)
+                                       .foregroundColor(.white)
+                                       .cornerRadius(31.5)
+                                       .sheet(isPresented: $isAccountTransferViewPresented) {
+                                           AccountTransferView(finalPrice: finalPrice, isPresented: $isAccountTransferViewPresented)
+                                       }
+                                
+
+                            }
+                            .padding()
+
                         }
                     }
                 }
